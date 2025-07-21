de87e81440dae1e2a24f3819141895408f3acf1c
Garfielddrey <henzeno45@gmail.com>
2025-07-21 17:44:48 +0300

    message commit change

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+
+import NavBar from './sections/NavBar';
+import Hero from './sections/Hero';
+import ComingSoon from './sections/ComingSoon';
+import FirstVideo from './sections/FirstVideo';
+import Jason from './sections/Jason';
+import SecondVideo from './sections/SecondVideo';
+import Lucia from './sections/Lucia';
+import PostCard from './sections/PostCard';
+import Final from './sections/Final';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  return (
+    <main>
+      <NavBar />
+      <Hero />
+      <ComingSoon />
+      
+      <FirstVideo />
+<Jason/>
+<SecondVideo/>
+<Lucia/>
+  <PostCard/>  
+  <Final/>
+    </main>
+  )
+}
+
+export default App;
---
+++ src/sections/PostCard.jsx
@@ -0,0 +1,42 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import React, { useRef } from 'react'
+
+const PostCard = () => {
+    const videoRef = useRef(null);
+    useGSAP(() => {
+        const tl = gsap.timeline({
+            scrollTrigger:{
+                trigger:'.postcard-card',
+                start:'top center',
+                end:'bottom center',
+                scrub:true,
+            }
+        })
+        videoRef.current.onloadedmetadata =() => {
+        tl.to(videoRef.current,{currentTime:videoRef.current.duration,
+            duration:3,ease:'power1.inOut'
+        },'<');
+        }
+    })
+    return (
+    <section className='post-card'>
+        <div className='animated-gradient-bg'>
+            <div className='post-card-wrapper  group hover:rotate-1 hover:-[1.02] transition duration-700'>
+                <img src="/images/overlay.webp" alt="" />
+            </div>
+            <video 
+            ref={videoRef}
+            muted
+            playsInline
+            preload='auto'
+            src='/videos/postcard-vd.mp4'/>
+            <button className='group-hover:bgyellow transation duration-700'>
+                Explore Leonida Keys 
+            </button>
+        </div>
+    </section>
+    )
+}
+
+export default PostCard
